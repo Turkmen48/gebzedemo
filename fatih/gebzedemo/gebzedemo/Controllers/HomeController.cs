1fc05002efbb3dd10a49d1cd77458bd59bc723de
fa7ih <fatiheneskilic9@gmail.com>
2022-08-07 12:29:49 +0300

    fatih dosyaları yükledi

---
+++ fatih/gebzedemo/gebzedemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using gebzedemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace gebzedemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Camiiler()
+        {
+            return View();
+        }
+        public IActionResult Cesmeler()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
