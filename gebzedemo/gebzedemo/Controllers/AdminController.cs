cdeea44cac43c43f1e2d7a9c2aa76a67da8a75fe
Turkmen48 <enesusta6638@gmail.com>
2022-08-08 22:39:28 +0300

    admin panel front end eklendi
    admin panel eklendi site adı gezgebze olarka değiştirildi layout düzenlendi ufak ayarlamalar yapıldı

---
+++ gebzedemo/gebzedemo/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gebzedemo.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
